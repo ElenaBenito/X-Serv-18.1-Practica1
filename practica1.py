27bc07332bda8fe070c3b26d528191b608a65e17
Elena Benito del Pino <elena.bnt5@gmail.com>
2018-03-13 17:17:37 +0100

    version 2 practica web acortadora

--- practica1.py
+++ practica1.py
@@ -25,6 +25,7 @@ class shortWebApp(webapp.webApp):
 		metodo, recurso, peticion = parsedRequest	
 		print("metodo =" + metodo)
 		print("recurso =" + recurso)
+#Habria que copiar el contenido de diccionario.csv en el diccionario si el fichero csv ya existe. Después hay que buscar en el diccionario si la url solicitada ya ha sido acortada.
 		if metodo == "GET":
 			if recurso != '/':		#Redireccionar al sitio
 				url_corta = "http://localhost:1234" + recurso
@@ -36,14 +37,21 @@ class shortWebApp(webapp.webApp):
 				except KeyError:
 					code = "404 NOT_FOUND"
 					body = "<html>""HTTP ERROR: Recurso no disponible""</html>"	
-			else:								#si no hay recurso, mostramos el formulario CON LA LISTA DE URLS ACORTADAS
+			else:								#si no hay recurso, mostramos el formulario y la lista de urls acortadas (archivo csv)
 				code = "200 OK"
-				body = "<html>Formulario<br><br>" + formulario + "<html><body><h1>Lista de URLs acortadas: </h1>" + csv.reader(self.__init__.myfile) + "</body></html>"
+				body = "<html><h1>Formulario<br><br></h1>" + formulario + "<html><body><br><br><h1>Lista de URLs acortadas: </h1></body></html>"
+				with open('diccionario.csv') as csvarchivo:
+					entrada = csv.reader(csvarchivo)
+					for reg in entrada:
+						line = str(reg)
+						linecsv = line.split(',')[0] + " -->" + line.split(',')[1]
+						body = body + "<html><body>" + linecsv + "<br></body></html>"
 		elif metodo == "POST":
 			url = peticion.split('\r\n\r\n',1)[1].split('=')[1]
 			url = urllib.parse.unquote(url, encoding='utf-8', errors='replace')
 			begin_url = url.split('://')[0] 
-			if begin_url != "http" or begin_url != "https":
+			print ("begin_url= "+begin_url)
+			if begin_url != "http" and begin_url != "https":
 				url = "http://" + url
 			short_url = "http://localhost:1234/" + str(self.num)
 			self.num += 1
@@ -52,13 +60,12 @@ class shortWebApp(webapp.webApp):
 			with open('diccionario.csv', 'a', newline='') as myfile:
 				newUrl = csv.writer(myfile).writerow([short_url, url])
 			code = "200 OK"
-			body = "<html>""URL real: "+ url +"<br><br>URL acortada: " + short_url + "</html>"
+			body = "<html>""URL real: <a href=>"+ url +"</a><br><br>URL acortada: <a href=>" + short_url + "</a></html>"
 		return(code, body)
 
 	def __init__(self, hostname, port):
 # Abrir el fichero que contiene el diccionario en formato separado por comas. 'x'-->crear y escribir; 'a'-->abrir para escribir al final de lo que tenga si está ya escrito
 		myfile = open('diccionario.csv', 'a')
-		
 		myfile.close()
 		super().__init__(hostname,port)
 
